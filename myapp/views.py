06c52bb048d410670c7f43cc372a27f640ad14e7
Alexey Zharinov <87263895+alzex3@users.noreply.github.com>
2022-01-31 17:28:09 +0300

    next

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from services.api import get_cad_data
+
 
 class MainView(TemplateView):
 
@@ -8,5 +10,8 @@ class MainView(TemplateView):
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         search_query = request.GET.get('search_box')
-        context['search'] = search_query
+        result = False
+        if search_query:
+            result = get_cad_data(search_query)
+        context['search'] = result
         return self.render_to_response(context)
